acdd39534fee8ed9deca2a5570cbb0ca1309fcc7
aditya mangal <adityamangal0202@gmail.com>
2021-08-20 23:30:22 +0530

    App files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Nav from "./components/Navbar"
+import Textinput from "./components/textinput"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Nav title="Get My Text" />
+      <div className="container">
+      <Textinput heading = "Get Your Text The Way You Want"/>
+      </div>
+    </>
+
   );
 }
 
