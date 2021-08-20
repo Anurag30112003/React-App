ac847fd8c7e444b5569662526fec6bf3afd2a069
aditya mangal <adityamangal0202@gmail.com>
2021-08-21 00:05:18 +0530

    more Text Function added

--- src/components/textinput.js
+++ src/components/textinput.js
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function textinput(props) {
+
+export default function Textinput(props) {
+
+    const ChangeText = () => {
+        let New = text.toUpperCase();
+        SetText(New)
+    }
+
+    const changeHandle = (event) => {
+        SetText(event.target.value)
+    }
+    const [text,SetText] = useState('')
     return (
         <div>
             <h1>
@@ -8,7 +19,8 @@ export default function textinput(props) {
             </h1>
             <div className="mb-3">
             <label for="exampleFormControlTextarea1" className="form-label">Type Below:</label>
-            <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                <textarea className="form-control" id="exampleFormControlTextarea1" rows="6" value={text} onChange={changeHandle}></textarea>
+                <button className="btn btn-primary my-2" onClick={ChangeText}>Click to Upper</button>
         </div>
     </div>
     )
